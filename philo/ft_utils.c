79645b48d1fb638cd07877e3eb31e631df3c686d
Valisoa Mirado Narindra Randrianoelisoa <vrandria@c1r2s3.42antananarivo.mg>
2024-07-23 09:01:23 +0300

    ok

---
+++ philo/ft_utils.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_utils.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vrandria <vrandria@student.42antananari    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/23 08:21:57 by vrandria          #+#    #+#             */
+/*   Updated: 2024/07/23 08:29:38 by vrandria         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+int	ft_atoi(const char *str)
+{
+	int		sign;
+	int		result;
+	int		i;
+
+	i = 0;
+	sign = 1;
+	result = 0;
+	while ((str[i] >= 9 && str[i] <= 13) || str[i] == 32)
+		i++;
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			sign *= -1;
+		i++;
+	}
+	while (str[i]>= '0' && str[i]<= '9')
+	{
+		result = (result * 10) + str[i] - '0';
+		i++;
+	}
+	return ((int)result * sign);
+}
---
+++ philo/philo.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vrandria <vrandria@student.42antananari    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/14 16:47:32 by vrandria          #+#    #+#             */
+/*   Updated: 2024/07/23 08:28:10 by vrandria         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+int	ft_atoi(const char *str);
+#endif
